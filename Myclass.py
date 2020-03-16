69da3bd6f488a4ae52dc95c1364057347a03ffab
k-yama007 <measuresoft@outlook.jp>
2020-03-16 17:32:25 +0900

    add file for class

---
+++ Myclass.py
@@ -0,0 +1,28 @@
+class Myclass:
+    """A simple example class"""
+    i = 12345
+
+    def f(self):
+        return 'hello world'
+    
+
+
+def __init__(self):
+    self.data=[]
+
+x=Myclass()
+y=Myclass
+
+
+print("x=Myclass()")
+print("Myclass():",x)
+print("Myclass.f:",x.f)
+print("Myclass.f():",x.f())
+print("Myclass.i:",x.i)
+print("y=Myclass:",y)
+
+
+if __name__ == "__main__" :
+    import sys
+
+    
