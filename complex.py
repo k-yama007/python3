68f07b02aba71805a813aee460f1ed8d49206971
k-yama007 <measuresoft@outlook.jp>
2020-03-16 17:48:54 +0900

    add file for complex

---
+++ complex.py
@@ -0,0 +1,12 @@
+class Complex:
+    def __init__(self,realpart,imagpart):
+        self.r = realpart
+        self.i = imagpart
+
+x = Complex(3.0,-4.5)
+x.r,x.i
+
+print("x=Complex(3.0,-4.5)")
+print("realpart(x.r):",x.r)
+print("imagpart(x.i):",x.i)
+
