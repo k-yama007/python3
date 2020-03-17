9dfb8d4d0de208deadc4481db6a36a9f362f26bf
k-yama007 <measuresoft@outlook.jp>
2020-03-17 12:57:53 +0900

    add class 9.4 iroiroreidai

---
+++ function.py
@@ -0,0 +1,23 @@
+#Function Defined outside the class
+def f1(self,x,y)
+    return min(x,x+y)
+
+class c:
+    f = f1
+
+    def g(self)
+        return 'hello world'
+    h = g
+
+class Bag:
+    def __init__(self):
+        self.data = []
+
+    def add(self,x):
+        self.data.append(x)
+
+    def addtwice(self, x):
+        self.add(x)
+        self.add(x)
+
+        
