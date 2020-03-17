17071f9aa82f2f03fce609a5be8f61f8ecb283cd
k-yama007 <measuresoft@outlook.jp>
2020-03-17 19:08:25 +0900

    add iterator

--- mapping.py
+++ mapping.py
@@ -35,5 +35,14 @@ print("name:",john.name)
 print("Dept:",john.dept)
 print("salary:",john.salary)
 
-
+for element in [1,2,3]:
+    print(element)
+for element in (1,2,3):
+    print(element)
+for key in {'one':1,'two':2}:
+    print(key)
+for char in "123":
+    print(char)
+for line in open("myfile.txt"):
+    print(line,end='')
     
