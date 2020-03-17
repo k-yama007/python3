f45d4bec466454ab9f49efa586ee4784c3d335cb
k-yama007 <measuresoft@outlook.jp>
2020-03-17 18:40:36 +0900

    add Employee Class

--- mapping.py
+++ mapping.py
@@ -18,4 +18,22 @@ class MappingSubclass(Mapping):
             self.items_list.append(item)
 
 
+class Employee:
+    pass
+
+
+john = Employee() #Create an empty employee record
+
+#Fill the fields of the record
+john.name = 'John Doe'
+john.dept='computer lab'
+john.salary = 1000
+
+
+
+print("name:",john.name)
+print("Dept:",john.dept)
+print("salary:",john.salary)
+
+
     
